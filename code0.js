0e3b56f200eb945f6e53be0d4153cbc6abdc8860
alin02912 <85516870+alin02912@users.noreply.github.com>
2021-11-21 22:36:10 -0500

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,20 @@
+gdjs.Scene_321Code = {};
+
+gdjs.Scene_321Code.conditionTrue_0 = {val:false};
+gdjs.Scene_321Code.condition0IsTrue_0 = {val:false};
+
+
+gdjs.Scene_321Code.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Scene_321Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Scene_321Code.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['Scene_321Code'] = gdjs.Scene_321Code;
